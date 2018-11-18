d60d437e5242d01133ce845c2e94167dd2198fbe
zwyu123 <zwyu2017@163.com>
2018-11-18 09:57:04 +0800

    项目初始化

---
+++ micro-weather-basic/src/main/java/com/spring/cloud/microweatherbasic/MicroWeatherBasicApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.microweatherbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroWeatherBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroWeatherBasicApplication.class, args);
+	}
+}
