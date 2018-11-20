29a506b85575e281a67e0834569b2dfa61279a36
zwyu123 <zwyu2017@163.com>
2018-11-21 00:07:00 +0800

    service与controller

--- micro-weather-basic/src/main/java/com/spring/cloud/weather/service/WeatherDataServiceImpl.java
+++ micro-weather-basic/src/main/java/com/spring/cloud/weather/service/WeatherDataServiceImpl.java
@@ -1,16 +1,51 @@
 package com.spring.cloud.weather.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.cloud.weather.vo.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+
+@Service
 public class WeatherDataServiceImpl implements WeatherDataService {
+    private static final String WEATHER_URI = "http://wthrcdn.etouch.cn/weather_mini?";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Override
     public WeatherResponse getDataByCityId(String cityId) {
-
-        return null;
+        String uri = WEATHER_URI + "citykey=" + cityId;
+        return this.doGetWeahter(uri);
     }
 
     @Override
     public WeatherResponse getDataByCityName(String cityName) {
-        return null;
+        String uri = WEATHER_URI + "city=" + cityName;
+        return this.doGetWeahter(uri);
     }
+
+
+    private WeatherResponse doGetWeahter(String uri) {
+        ResponseEntity<String> respString = restTemplate.getForEntity(uri, String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        WeatherResponse resp = null;
+        String strBody = null;
+
+        if (respString.getStatusCodeValue() == 200) {
+            strBody = respString.getBody();
+        }
+
+        try {
+            resp = mapper.readValue(strBody, WeatherResponse.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resp;
+    }
+
 }
