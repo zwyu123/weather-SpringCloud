6d28bdf804e1fe6bb8d76bbaeb9d0e40bb79b988
zwyu123 <zwyu2017@163.com>
2018-12-18 11:06:31 +0800

    City

---
+++ micro-weather-quartz/src/main/java/com/spring/cloud/weather/vo/CityList.java
@@ -0,0 +1,22 @@
+package com.spring.cloud.weather.vo;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "c")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CityList {
+    @XmlAttribute(name = "d")
+    private List<City> cityList;
+
+    public List<City> getCityList() {
+        return cityList;
+    }
+
+    public void setCityList(List<City> cityList) {
+        this.cityList = cityList;
+    }
+}
